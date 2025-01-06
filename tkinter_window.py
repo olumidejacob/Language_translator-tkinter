976a10043413c8835226618f2783e8031d227086
olumidejacob <olumideaji101@gmail.com>
2025-01-06 15:39:51 -0800

    jacob_french dictionary

---
+++ tkinter_window.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+# Dictionary of languages and their translations
+languages = {
+    "French": {
+        "Yes": "Oui",
+        "goodbye": "Au revoir",
+        "Help":"Aide",
+        "thank you": "Merci",
+        "please": "S'il vous plaît",
+        "yes": "Oui",
+        "no": "Non",
+        "Mother":"Mere",
+        "Excuse me":"Excusez-moi",
+        "No":"Non",
+        "Menu":"Menu",
+        "computer":"Ordinateur",
+        "Trip":"Voyage",
+        "Good luck":"Bonne nuit",
+        "see you soon":"A bientot",
+        "see you later":"A plus Tard",
+        "Father":"Frere",
+        "Sorry":"desole(e)",
+        "how are you":"Comment ca va",
+        "Your welcome":"De rien"
+
+    },
+}
+
